964332dbfe9f303881e4af386ef62784205e9cb1
sp-xd <69642436+SP-XD@users.noreply.github.com>
2023-04-14 02:34:17 +0530

    initial bringup

---
+++ android/app/src/main/kotlin/com/example/socket_rocket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.socket_rocket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/message_model.dart
@@ -0,0 +1,10 @@
+class MessageData {
+  String? message;
+  String? username;
+  bool? isme;
+
+  MessageData({
+    required this.message,
+    required this.isme
+  });
+}
